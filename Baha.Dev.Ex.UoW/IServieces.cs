47a7a38325034e8c5a0303f415372680437ddb59
BaHa.TR <eng.baha.tr@gmail.com>
2024-10-14 10:05:50 +0300

    Add project files.

---
+++ Baha.Dev.Ex.UoW/IServieces.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace BahaDev.EX.UoW
+{
+    public interface IServieces<T> where T : class
+    {
+        // إرجاع جميع الكيانات
+        Task<List<T>> GetAll();
+
+        // إرجاع كيان واحد بناءً على معرف (ID)
+        Task<T?> GetById(Guid id);
+
+        // إضافة كيان جديد
+        Task<T> Insert(T entity);
+
+        // تحديث كيان موجود
+        Task<T> Update(T entity);
+
+        // حذف كيان بناءً على معرف (ID)
+        Task<bool> Delete(Guid id);
+    }
+}
+
---
+++ Baha.Dev.Ex.UoW/RServieces.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BahaDev.EX.UoW
+{
+    public class RServieces <T , TContext> : IServieces<T> where T : class where TContext : DbContext
+    {
+        private readonly TContext _context;
+
+        public RServieces(TContext context)
+        {
+            _context = context;
+        }
+
+        // إرجاع جميع الكيانات
+        public async Task<List<T>> GetAll()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        // إرجاع كيان واحد بناءً على معرف (ID)
+        public async Task<T?> GetById(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        // إضافة كيان جديد
+        public async Task<T> Insert(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        // تحديث كيان موجود
+        public async Task<T> Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        // حذف كيان بناءً على معرف (ID)
+        public async Task<bool> Delete(Guid id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity == null)
+            {
+                return false;
+            }
+
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
